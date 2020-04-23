33c681ab4b08a565bf34083a54425b839de51b94
codersave <you@example.com>
2020-04-23 12:24:51 +0800

    new commit

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend :recommends="recommends"/>
+    <featrue/>
+    
+    <!-- ul>li{列表$}*50 -->
+
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
+  import HomeSwiper from './childComps/HomeSwiper'
+  import Recommend from './childComps/Recommend'
+  import Featrue from './childComps/Featrue'
 
   import { getHomeMultidata } from 'network/home'
 
   export default {
     name: "Home",
     components: {
-      NavBar
+      NavBar,
+      HomeSwiper,
+      Recommend,
+      Featrue
     },
     data(){
       return {
@@ -30,8 +42,17 @@
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav {
     color: #fff;
     background-color: var(--color-tint);
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 9;
   }
 </style>
